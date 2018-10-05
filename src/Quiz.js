c7c119be8af561f5af4843af079190e95a490bb8
Unknown <15042658+cjw21824@users.noreply.github.com>
2018-10-05 12:25:14 -0700

    Complete Module
    complete the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -4,9 +4,13 @@ let quizData= require('./quiz_data.json');
 
 class Quiz extends Component
 {
-    state={
-        quiz_position: 1
+    constructor(props) {
+        super(props);
+        this.state={
+           quiz_position: 1 
+        }
     }
+    
     render(){
         return (
             <div>
@@ -20,4 +24,4 @@ class Quiz extends Component
 }
 
 
-export default Quiz;
+export default Quiz
