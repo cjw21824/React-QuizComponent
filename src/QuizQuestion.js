c0b9a10b47e1b657769daeec7226244a5f4d8a7d
Unknown <15042658+cjw21824@users.noreply.github.com>
2018-10-06 22:46:56 -0700

    Completed Module 6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,7 +5,9 @@ class QuizQuestion extends Component
 {
     constructor(props){
         super(props);
-        this.state = {}
+        this.state = {
+            incorrectAnswer: false
+        }
         
     }
 
@@ -14,7 +16,9 @@ class QuizQuestion extends Component
         if(buttonText === this.props.quiz_question.answer)
         { 
             this.props.showNextQuestionHandler()
+            this.setState({incorrectAnswer: false})
         }
+        this.setState({incorrectAnswer: true})
     }
 
     render() {
@@ -28,6 +32,7 @@ class QuizQuestion extends Component
                 {this.props.quiz_question.answer_options.map((answer_option, i) => ( <QuizQuestionButton key={i} clickHandler={this.handleClick.bind(this)} button_text={answer_option}/> ))}
               </ul>
             </section>
+            {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : <p>null</p>}
           </main>
         )
     }
